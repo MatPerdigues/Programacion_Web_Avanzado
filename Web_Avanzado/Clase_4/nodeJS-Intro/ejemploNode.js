dd15efebd89245d86e95dd3ed903f793e0918efb
MatPerdigues <you@example.com>
2023-03-23 03:12:35 -0300

    Clase_4.B

---
+++ Web_Avanzado/Clase_4/nodeJS-Intro/ejemploNode.js
@@ -0,0 +1,9 @@
+const sumar = (num1,num2)=>{
+    return num1+num2;
+}
+
+const SALUDO = (nombre)=>{
+    console.log("Hola " + nombre);
+}
+
+module.exports={sumar,SALUDO}; //importante poner el export al final del archivo.
