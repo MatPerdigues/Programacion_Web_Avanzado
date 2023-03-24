59fe07f93e92ad41062a10cc282c0c2eb0414936
MatPerdigues <you@example.com>
2023-03-24 03:23:28 -0300

    Clase_5

---
+++ Web_Avanzado/Clase_5/TS/EjercicioTS.ts
@@ -0,0 +1,27 @@
+/*class Producto {
+    name:string;
+    price:string;
+    year:string
+    constructor(nombre:string,precio:string,anio:string){
+        this.name=nombre;
+        this.price=precio;
+        this.year=anio
+    }
+}
+
+class Ente {
+    mostrarRecuadro(){
+        let zonaProductos = document.querySelector("#listaProductos");
+        let contenedor = document.createElement("p");
+        contenedor.innerText="un producto";
+    }
+}
+
+const enviarFormulario = (event) =>{
+    event.preventDefault();
+    let nomProd = document.getElementById("name")?.value;
+    let precProd = document.getElementById("price")?.value;
+    let anioProd = document.getElementById("year")?.value;
+    let productoNuevo = new Producto(nomProd,precProd,anioProd);
+
+}*/
